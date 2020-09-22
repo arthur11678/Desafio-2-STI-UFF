6e2c3edc95ff870748492688ff34efc0324f45ae
Arthur Pinheiro <arthurpm@id.uff.br>
2020-09-22 16:46:27 -0300

    Correção de erros

--- js/app.js
+++ js/app.js
@@ -6,45 +6,72 @@ function formSubmit() {
   // console.log(endereco, cidade);
 
   // Pega a Latitude e Longitude
-  let localizacao = pegarLocalizacao(endereco, cidade);
+  let localizacao = [];
+  pegarLocalizacao(endereco, cidade).then((data) => {
+    localizacao = data;
+    console.log(localizacao);
+  });
 
   // Pega Lista de todas as Estações
-  let todasEstacoes = buscarEstacoes();
+  let todasEstacoes = [];
+  buscarEstacoes().then((data) => {
+    todasEstacoes = data;
+    console.log(todasEstacoes);
+  });
+  // .then(() => {});
 
   // Pega a Localização da Estação mais perto do endereço
-  let estacaoMaisPerto = estacaoMaisPerto(
+  let estacaoMaisPerto = [];
+  estacaoMaisPerto = calcularEstacaoMaisPerto(
     todasEstacoes,
     localizacao[0],
     localizacao[1]
   );
 
-  console.log(estacaoMaisPerto);
+  // console.log(estacaoMaisPerto);
 }
 
-function pegarLocalizacao(endereco, cidade) {
-  let localizacao = [];
-  // Lógica da Requisição Json
+async function pegarLocalizacao(endereco, cidade) {
+  let response = await fetch(
+    "https://nominatim.openstreetmap.org/search?street=" +
+      endereco +
+      "&city=" +
+      cidade +
+      "&county=Brasil&format=json"
+  );
+
+  response = await response.json();
+
+  let localizacao = [response[0]["lat"], response[0]["lon"]];
+
   return localizacao;
 }
 
-function buscarEstacoes() {
-  let todasEstacoes = [];
-  let response = await fetch('http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/estacoesBikeRio');
-  if(response.ok){
-    let todasEstacoes = await response.json();
-  }else{
-    alert("HTTP-Error: " + response.status)
-  }
+async function buscarEstacoes() {
+  let response = await fetch(
+    "http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/estacoesBikeRio"
+  );
+
+  response = await response.json();
+
+  let todasEstacoes = response["DATA"];
+
   return todasEstacoes;
 }
 
 function calcularDistacia(latitude_1, longitude_1, latitude_2, longitude_2) {
   let distancia = 0;
+  distancia = Math.hypot(latitude_2 - latitude_1, longitude_2 - longitude_1);
   return distancia;
 }
 
-function estacaoMaisPerto(todasEstacoes, latitude, longitude) {
+function calcularEstacaoMaisPerto(todasEstacoes, latitude, longitude) {
   let estacaoMaisPerto = [];
-  // Lógica de Cálcular a Distância entre os pontos
+  estacaoMaisPerto = calcularDistacia(
+    todasEstacoes[5],
+    todasEstacoes[6],
+    latitude,
+    longitude
+  );
   return estacaoMaisPerto;
 }
